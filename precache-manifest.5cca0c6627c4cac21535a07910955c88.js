b4cf231701db8a8fd3b316ede2360dc195735bce
Elior Avramoviz <elioravr@gmail.com>
2019-09-28 12:22:03 +0300

    Update the resume to 2019

---
+++ precache-manifest.5cca0c6627c4cac21535a07910955c88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "524d1c69cdfe454507409e2e2be5d0b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3489912fe99baf70b6f1",
+    "url": "/static/css/main.fac0fc00.chunk.css"
+  },
+  {
+    "revision": "cd023c827d15b4fabf38",
+    "url": "/static/js/2.2fb57ecf.chunk.js"
+  },
+  {
+    "revision": "3489912fe99baf70b6f1",
+    "url": "/static/js/main.a763340a.chunk.js"
+  },
+  {
+    "revision": "a47e7e47fb6e4035e86e",
+    "url": "/static/js/runtime-main.150d68ae.js"
+  }
+]);
